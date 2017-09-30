689607339659e8ab371eeb7b17f4d21a528a2e2f
bigspotteddog <bink.lynch@gmail.com>
2017-09-29 18:11:16 -0700

    Updated to cookbook url to new repo and version. Changed to hyphens to underscore per changes to the cookbook. Added rspec tests.

--- Dockerfile
+++ Dockerfile
@@ -40,12 +40,14 @@ ENV NEXUS_HOME=${SONATYPE_DIR}/nexus \
     NEXUS_CONTEXT='' \
     SONATYPE_WORK=${SONATYPE_DIR}/sonatype-work
 
+ENV NEXUS_REPOSITORY_MANAGER_COOKBOOK_VERSION="release-0.2.0-01"
+
 ADD solo.json.erb /var/chef/solo.json.erb
 
 # Install using chef-solo
 RUN curl -L https://www.getchef.com/chef/install.sh | bash \
     && /opt/chef/embedded/bin/erb /var/chef/solo.json.erb > /var/chef/solo.json \
-    && chef-solo --recipe-url https://s3.amazonaws.com/int-public/nexus-repository-manager-cookbook.tar.gz --json-attributes /var/chef/solo.json \
+    && chef-solo --recipe-url https://github.com/sonatype/chef-nexus-repository-manager/releases/download/${NEXUS_REPOSITORY_MANAGER_COOKBOOK_VERSION}/chef-nexus-repository-manager.tar.gz --json-attributes /var/chef/solo.json \
     && rpm -qa *chef* | xargs rpm -e \
     && rpm --rebuilddb \
     && rm -rf /etc/chef \
---
+++ spec/Dockerfile_spec.rb
@@ -0,0 +1,43 @@
+# Copyright (c) 2017-present Sonatype, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "serverspec"
+require "docker"
+
+describe 'Dockerfile' do
+  before(:all) do
+    Docker.options[:read_timeout] = 900
+    @image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :redhat
+    set :backend, :docker
+    set :docker_image, @image.id
+  end
+
+  it 'should remove solo.json during cleanup' do
+    expect(File).not_to exist('/var/chef/solo.json')
+  end
+
+  it 'should not have a chef package installed' do
+    expect(package('chef')).not_to be_installed
+  end
+
+  it 'should have a user named nexus' do
+    expect(user('nexus')).to exist
+  end
+
+  it 'should have a nexus process running' do
+    expect(process('nexus')).to be_running
+  end
+end
