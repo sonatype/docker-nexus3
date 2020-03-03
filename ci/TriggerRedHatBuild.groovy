6dee7864336dc85c94783853cb5dbd3c7f3b2065
John Flinchbaugh <jflinchbaugh@sonatype.com>
2020-03-03 16:19:05 -0500

    wrap it all up in a class

--- ci/TriggerRedHatBuild.groovy
+++ ci/TriggerRedHatBuild.groovy
@@ -17,144 +17,154 @@ if (args.size() < 3) {
   fail('Usage: groovy TriggerRedhatBuild.groovy <version> <projectId> <apiKey>')
 }
 
-def (version, projectId, apiKey) = args
+new BuildClient(version: args[0], projectId: args[1], apiKey: args[2]).run()
 
-final HttpBuilder builder = HttpBuilder.configure {
-  request.uri = 'https://connect.redhat.com'
-  request.headers['Authorization'] = "Bearer ${apiKey}"
-  request.contentType = 'application/json'
-  request.body = [:]
-}
+class BuildClient {
+  String version
+  String projectId
+  String apiKey
 
-/* a function for querying all the tags */
-final Closure tagFn = this.&getTags.curry(builder, projectId)
-
-final nextTag = getNextTag(tagFn, version)
-println "Triggering build as ${nextTag}"
-
-final buildStatus = build(builder, projectId, nextTag)
-
-if (buildStatus.status != 'Created') {
-  fail(buildStatus)
-}
-
-final completedBuild = getCompletedBuild(tagFn, nextTag)
-
-final published = publish(builder, projectId, completedBuild.digest, completedBuild.name)
-
-if (published.failure) {
-  fail(published.failure)
-}
-
-println published
-
-// END
-
-/**
- * fail with message and exit with an error code for jenkins to see
- * @param message message to print
- */
-void fail(String message) {
-  System.err.println(message)
-  System.exit(1)
-}
-
-
-/**
- * Request current version tags available at Red Hat.
- * @param builder the configured http builder to use for requests
- * @param projectId project to query versions
- * @return the list of all tags
- */
-List getTags(HttpBuilder builder, String projectId) {
-  return builder.post {
-    request.uri.path = "/api/v2/projects/${projectId}/tags"
-  }.tags
-}
-
-/**
- * Request current version tags available at Red Hat,
- * and calculate the next tag to use in this build.
- * @param requestTags a closure that produces a list of all the tags
- * @param version the base version we're currently building
- * @return the full new version string to submit for the next build
- */
-String getNextTag(Closure requestTags, String version) {
-  final tags = requestTags()*.name.collectMany {
-    it.split(', ').collect()
-  }
-
-  final currentIndex = tags.findAll {
-    it.startsWith(version)
-  }.collect {
-    it.replaceAll(/${version}-(\d+)-?.*/, '$1') as Integer
-  }.sort().reverse()[0]
-
-  final nextIndex =((currentIndex ?: 0) as Integer) + 1
-
-  return "${version}-${nextIndex}"
-}
-
-/**
- * Trigger build of the certified image at Red Hat,
- * @param builder the configured http builder to use for requests
- * @param projectId project to build
- * @param nextTag the full version tag to be assigned to the new build
- * @return the map from json with the status of the submitted build
- */
-Map build(HttpBuilder builder, String projectId, String nextTag) {
-  return builder.post {
-    request.uri.path = "/api/v2/projects/${projectId}/build"
-    request.body = [tag: nextTag]
-  }
-}
-
-/**
- * Poll for the completed (built and scanned) build at Red Hat build service.
- * @param requestTags a closure that produces a list of all the tags
- * @param nextTag the full version tag assigned to the new build
- * @return the map from json with info about the completed build
- */
-Map getCompletedBuild(Closure requestTags, String nextTag) {
-  while (true) {
-    println 'Waiting for build to finish.'
-    sleep 60000
-
-    final completedBuild = requestTags().find {
-      it.name == nextTag && it.scan_status == 'passed'
+  /**
+   * fire off a series of requests to build and publish
+   * a container.
+   */
+  void run() {
+    final HttpBuilder builder = HttpBuilder.configure {
+      request.uri = 'https://connect.redhat.com'
+      request.headers['Authorization'] = "Bearer ${apiKey}"
+      request.contentType = 'application/json'
+      request.body = [:]
     }
 
-    if (completedBuild) {
-      return completedBuild
+    /* a function for querying all the tags */
+    final Closure tagFn = this.&getTags.curry(builder, projectId)
+
+    final nextTag = getNextTag(tagFn, version)
+    println "Triggering build as ${nextTag}"
+
+    final buildStatus = build(builder, projectId, nextTag)
+
+    if (buildStatus.status != 'Created') {
+      fail(buildStatus)
     }
+
+    final completedBuild = getCompletedBuild(tagFn, nextTag)
+
+    final published = publish(builder, projectId, completedBuild.digest, completedBuild.name)
+
+    if (published.failure) {
+      fail(published.failure)
+    }
+
+    println published
   }
-}
 
-/**
- * Trigger publishing of the new image at Red Hat build service.
- * @param builder the configured http builder to use for requests
- * @param projectId project to publish
- * @param digest hash string that identifies the container to publish
- * @param name tag name (version) of the container image to publish
- * @return the map from json with status of the published container image
- */
-Map publish(HttpBuilder builder, String projectId, String digest, String name) {
-  final publishPath = [
-    '/api/v2/projects',
-    projectId,
-    'containers',
-    digest,
-    'tags',
-    name,
-    'publish'
-  ].join('/')
+  /**
+  * fail with message and exit with an error code for jenkins to see
+  * @param message message to print
+  */
+  private void fail(String message) {
+    System.err.println(message)
+    System.exit(1)
+  }
 
-  try {
+
+  /**
+  * Request current version tags available at Red Hat.
+  * @param builder the configured http builder to use for requests
+  * @param projectId project to query versions
+  * @return the list of all tags
+  */
+  private List getTags(HttpBuilder builder, String projectId) {
     return builder.post {
-      request.uri.path = publishPath
+      request.uri.path = "/api/v2/projects/${projectId}/tags"
+    }.tags
+  }
+
+  /**
+  * Request current version tags available at Red Hat,
+  * and calculate the next tag to use in this build.
+  * @param requestTags a closure that produces a list of all the tags
+  * @param version the base version we're currently building
+  * @return the full new version string to submit for the next build
+  */
+  private String getNextTag(Closure requestTags, String version) {
+    final tags = requestTags()*.name.collectMany {
+      it.split(', ').collect()
     }
-  } catch (HttpException ex) {
-    ex.printStackTrace()
-    return [failure: "Failed to publish: ${ex.statusCode} [${ex.body}]"]
+
+    final currentIndex = tags.findAll {
+      it.startsWith(version)
+    }.collect {
+      it.replaceAll(/${version}-(\d+)-?.*/, '$1') as Integer
+    }.sort().reverse()[0]
+
+    final nextIndex =((currentIndex ?: 0) as Integer) + 1
+
+    return "${version}-${nextIndex}"
+  }
+
+  /**
+  * Trigger build of the certified image at Red Hat,
+  * @param builder the configured http builder to use for requests
+  * @param projectId project to build
+  * @param nextTag the full version tag to be assigned to the new build
+  * @return the map from json with the status of the submitted build
+  */
+  private Map build(HttpBuilder builder, String projectId, String nextTag) {
+    return builder.post {
+      request.uri.path = "/api/v2/projects/${projectId}/build"
+      request.body = [tag: nextTag]
+    }
+  }
+
+  /**
+  * Poll for the completed (built and scanned) build at Red Hat build service.
+  * @param requestTags a closure that produces a list of all the tags
+  * @param nextTag the full version tag assigned to the new build
+  * @return the map from json with info about the completed build
+  */
+  private Map getCompletedBuild(Closure requestTags, String nextTag) {
+    while (true) {
+      println 'Waiting for build to finish.'
+      sleep 60000
+
+      final completedBuild = requestTags().find {
+        it.name == nextTag && it.scan_status == 'passed'
+      }
+
+      if (completedBuild) {
+        return completedBuild
+      }
+    }
+  }
+
+  /**
+  * Trigger publishing of the new image at Red Hat build service.
+  * @param builder the configured http builder to use for requests
+  * @param projectId project to publish
+  * @param digest hash string that identifies the container to publish
+  * @param name tag name (version) of the container image to publish
+  * @return the map from json with status of the published container image
+  */
+  private Map publish(HttpBuilder builder, String projectId, String digest, String name) {
+    final publishPath = [
+      '/api/v2/projects',
+      projectId,
+      'containers',
+      digest,
+      'tags',
+      name,
+      'publish'
+    ].join('/')
+
+    try {
+      return builder.post {
+        request.uri.path = publishPath
+      }
+    } catch (HttpException ex) {
+      ex.printStackTrace()
+      return [failure: "Failed to publish: ${ex.statusCode} [${ex.body}]"]
+    }
   }
 }
