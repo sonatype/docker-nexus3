c3d8481ffdfc2c8756607bb82a45208cab3a4802
Sonatype <sonatype-ci@sonatype.com>
2023-07-21 22:08:08 +0000

    Update Repository Manager to 3.58.1-01.

--- Dockerfile.rh.el
+++ Dockerfile.rh.el
@@ -17,8 +17,8 @@ FROM registry.access.redhat.com/rhel7/rhel
 LABEL name="Nexus Repository Manager" \
       maintainer="Sonatype <support@sonatype.com>" \
       vendor=Sonatype \
-      version="3.57.1-02" \
-      release="3.57.1" \
+      version="3.58.1-01" \
+      release="3.58.1" \
       url="https://sonatype.com" \
       summary="The Nexus Repository Manager server \
           with universal support for popular component formats." \
@@ -36,9 +36,9 @@ LABEL name="Nexus Repository Manager" \
       io.openshift.expose-services="8081:8081" \
       io.openshift.tags="Sonatype,Nexus,Repository Manager"
 
-ARG NEXUS_VERSION=3.57.1-02
+ARG NEXUS_VERSION=3.58.1-01
 ARG NEXUS_DOWNLOAD_URL=https://download.sonatype.com/nexus/3/nexus-${NEXUS_VERSION}-unix.tar.gz
-ARG NEXUS_DOWNLOAD_SHA256_HASH=bd74daaf91194fced2bed2dd4b6eb477d2afb235bf23c2a3589db6c46f63551a
+ARG NEXUS_DOWNLOAD_SHA256_HASH=325d187ea6b3fd341f010570089b4c84f36aba217da45d1a4af634e1df3eba42
 
 # configure nexus runtime
 ENV SONATYPE_DIR=/opt/sonatype
